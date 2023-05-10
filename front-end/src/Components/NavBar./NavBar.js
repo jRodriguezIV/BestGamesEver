01ac1d4034b10c321ca5161a7cc4cc9a6f156061
jRodriguezIV <j.a.rodrigueziv@gmail.com>
2023-05-10 19:33:49 -0400

    commit before push

--- front-end/src/Components/NavBar./NavBar.js
+++ front-end/src/Components/NavBar./NavBar.js
@@ -1,10 +1,15 @@
 import './NavBar.css'
+import { Link } from 'react-router-dom'
+
 
 export default function NavBar() {
 
-    return (
-        <div className='NavBar'>
-            <p>This is the NavBar</p>
+    return ( 
+       <nav class="navbar navbar-expand-lg bg-body-tertiary" style={{backgroundColor: "#F24822"}}>
+        <div class="container-fluid">
+        <Link class="navbar-brand" to="/"><strong>BestGamesEver</strong></Link>
+        <Link to="/new" class="nav-link active">New</Link>
         </div>
+       </nav>
     )
 }
